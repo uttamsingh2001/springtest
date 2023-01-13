3e9b2f69877a2872ef99761904b1a762e7d117e4
UTTAM SINGH <uttamsingh9022296913@gmail.com>
2023-01-13 11:38:36 +0530

    spring test

---
+++ src/main/java/com/example/springjenkin/SpringjenkinApplication.java
@@ -0,0 +1,16 @@
+package com.example.springjenkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjenkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringjenkinApplication.class, args);
+
+
+		System.out.println("Hello");
+	}
+
+}
